39a893f02d70d4d614b46cb599b0f13e59c7da82
Jeff Olson <olson.jeffery@gmail.com>
2014-09-12 08:41:23 -0500

    impl + tests for stringToTokens()

--- src/parse.js
+++ src/parse.js
@@ -1,4 +1,56 @@
-define([], function() {
-    return function(input) {
+// This module concerns the takes of taking a string,
+// representing the "human readable" (ie code)
+// representing of an S-Expression and converting to a
+// representation (ie data) that is usable by the dawdle
+// interpreter  
+define(['src/expr'], function(expr) {
+    "use strict";
+    // ### stringToTokens()
+    // 
+    // Takes in a string argument, `inputStr`, and parses
+    // it into an array of individual "token" strings, which
+    // of individual items that can be read and converted
+    // into an S-Expression
+    var stringToTokens = function(inputStr) {
+        if (typeof inputStr !== 'string') {
+            throw "stringToTokens() expects a string argument";
+        }
+
+        // Run through the provided string, building a new string with padding around brackets
+        var tmpStr = ''
+        var last = '';
+        var curr = '';
+        for(var ctr = 0; ctr < inputStr.length;ctr++) {
+            var curr = inputStr[ctr];
+            if (curr === '(' || curr === ')') {
+                tmpStr += ' '+curr+' ';
+            } else {
+                tmpStr += curr;
+            }
+        }
+        // trim whitepsace and return an array of the input, split on whitespace
+        return tmpStr.trim().split(/\s+/)
     };
+    
+    // ### tokensToExpr()
+    //
+    // Takes in an array of `tokens` and moves through
+    // them, recursively building up and returning a
+    // single S-Expression
+    var tokensToExpr = function(tokens) {
+    };
+
+    // ### parse()
+    //
+    // The publically exported function that orchestrates
+    // the previous steps.
+    var parse = function(input) {
+    };
+
+    // _IMPL NOTE:_ All of the individual functions
+    // that make up the orchestration are attached to it
+    // for ease of testing (while still returning a single,
+    // function from the module)
+    parse.stringToTokens = stringToTokens;
+    return parse;
 });
--- src/tests/parse-tests.js
+++ src/tests/parse-tests.js
@@ -5,4 +5,30 @@ define(['chai', 'src/parse'], function(chai, parse) {
             (typeof parse).should.equal('function');
         });
     });
+    suite('parse:stringToTokens', function() {
+        test('stringToTokens() is a function', function() {
+            (typeof parse.stringToTokens).should.equal('function');
+        });
+        test('stringToTokens() throws with non-string argument', function() {
+            should.Throw(function() {
+                parse.stringToTokens([]);
+            });
+        });
+        test('stringToTokens() converts "42" to a single token result', function() {
+            var result = parse.stringToTokens('42');
+            result.length.should.equal(1);
+            result[0].should.equal('42');
+        });
+        test('stringToTokens() converts "( list 1 2 )" to a 5 token result', function() {
+            var result = parse.stringToTokens('( list 1 2 )');
+            console.log(result);
+            result.length.should.equal(5);
+        });
+        test('stringToTokens() converts "(foo (42))" to a 6 token result', function() {
+            var result = parse.stringToTokens('(foo (42))');
+            result.length.should.equal(6);
+        });
+    });
+    suite('parse:read', function() {
+    });
 });
